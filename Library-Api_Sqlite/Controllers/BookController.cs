a25d9a0c57141986f67fda7c608cb37f46f0214b
Jathushan <jathushanj2003@gmail.com>
2024-10-07 19:56:37 +0530

    correct all book api methods

--- Library-Api_Sqlite/Controllers/BookController.cs
+++ Library-Api_Sqlite/Controllers/BookController.cs
@@ -1,10 +1,7 @@
 ﻿using Library_Api_Sqlite.Dto_s;
 using Library_Api_Sqlite.EntityModals;
 using Library_Api_Sqlite.Services;
-
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Data.Sqlite;
 
 namespace Library_Api_Sqlite.Controllers
 {
@@ -12,148 +9,113 @@ namespace Library_Api_Sqlite.Controllers
     [ApiController]
     public class BookController : ControllerBase
     {
-        private readonly string _connectionString;
-        BookService _bookservics;
+        private readonly BookService _bookservics;
 
-        public BookController(string connectionString, BookService bookservics)
+        public BookController(BookService bookservics)
         {
-            _connectionString = connectionString;
             _bookservics = bookservics;
         }
 
-
-        [HttpPost ("Addnewbook")]
+        [HttpPost("AddNewBook")]
         public async Task<IActionResult> Add(Book_Req_Dto book)
         {
-            var mainBook = await _bookservics.AddBook(book);
-            if (mainBook == null)
+            try
             {
-                return BadRequest("Unable to add the book.");
+                var mainBook = await _bookservics.AddBook(book);
+                return CreatedAtAction(nameof(GetBooK), new { isbn = mainBook.ISBN }, mainBook);
             }
-            return Ok(mainBook);
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
         }
 
-        [HttpGet("GetAllbook")]
-        public async Task<IEnumerable<Book>> GetAllBooks()
+        [HttpGet("GetAllBooks")]
+        public async Task<IActionResult> GetAllBooks()
         {
-            return await _bookservics.GetAllBooks();
+            var books = await _bookservics.GetAllBooks();
+            return Ok(books);
         }
 
         [HttpGet("GetBook")]
-        public async Task<Book> GetBooK(string isbn)
+        public async Task<IActionResult> GetBooK(string isbn)
         {
-            return await _bookservics.GetBook(isbn);
+            try
+            {
+                var book = await _bookservics.GetBook(isbn);
+                return Ok(book);
+            }
+            catch (Exception ex)
+            {
+                return NotFound(new { message = ex.Message });
+            }
         }
 
-        [HttpDelete ("deleteBook")]
-        public async Task<bool> DeleteById(int id)
+        [HttpDelete("DeleteBook")]
+        public async Task<IActionResult> DeleteById(int id)
         {
-            return await _bookservics.DeleteById(id);
+            var deleted = await _bookservics.DeleteById(id);
+            if (!deleted)
+            {
+                return NotFound(new { message = "Book not found!" });
+            }
+            return NoContent();
         }
 
         [HttpGet("CountTotalBooks")]
-        public async Task<int> CountTotalBooks()
+        public async Task<IActionResult> CountTotalBooks()
         {
-            return await _bookservics.CountTotalBooks();
+            var count = await _bookservics.CountTotalBooks();
+            return Ok(count);
         }
 
-
         [HttpGet("GetAllPublishYears")]
-        public async Task<List<int>> GetAllPublishYears()
+        public async Task<IActionResult> GetAllPublishYears()
         {
-            return await _bookservics.GetAllPublishYears();
+            var years = await _bookservics.GetAllPublishYears();
+            return Ok(years);
         }
 
         [HttpGet("GetAllGenres")]
-        public async Task<List<string>> GetAllGenres()
+        public async Task<IActionResult> GetAllGenres()
         {
-            return await _bookservics.GetAllGenres();
+            var genres = await _bookservics.GetAllGenres();
+            return Ok(genres);
         }
 
         [HttpGet("GetAllAuthors")]
-        public async Task<List<string>> GetAllAuthors()
+        public async Task<IActionResult> GetAllAuthors()
         {
-            return await _bookservics.GetAllAuthors();
+            var authors = await _bookservics.GetAllAuthors();
+            return Ok(authors);
         }
 
-        [HttpGet("GetBooksByPublishYear")]
-        public async Task<List<Book>> GetBooksByPublishYear(string publishYear)
+        [HttpGet("GetByPublishYear")]
+        public async Task<IActionResult> GetBooksByPublishYear(int year)
         {
-            return await GetBooksByPublishYear(publishYear);
+            var books = await _bookservics.GetBooksByPublishYear(year);
+            return Ok(books);
         }
 
-        [HttpGet("GetBooksByGenre")]
-        public async Task<List<string>> GetBooksByGenre(string genre)
+        [HttpGet("GetByGenre")]
+        public async Task<IActionResult> GetBooksByGenre(string genre)
         {
-            return await GetBooksByGenre(genre);
+            var books = await _bookservics.GetBooksByGenre(genre);
+            return Ok(books);
         }
 
-        [HttpGet("GetBooksOrderedByPublishYear")]
-        public async Task<List<Book>> GetBooksOrderedByPublishYear(bool ascending)
+        [HttpGet("SearchByTitle")]
+        public async Task<IActionResult> SearchBooksByTitle(string title)
         {
-            return await GetBooksOrderedByPublishYear(ascending);
+            var books = await _bookservics.SearchBooksByTitle(title);
+            return Ok(books);
         }
 
-
-
-        [HttpGet("SearchBooksByTitle")]
-        public async Task<List<Book>> SearchBooksByTitle(string title)
+        [HttpGet("GetOrderedByPublishYear")]
+        public async Task<IActionResult> GetBooksOrderedByPublishYear(bool ascending = true)
         {
-            return await SearchBooksByTitle(title);
+            var books = await _bookservics.GetBooksOrderedByPublishYear(ascending);
+            return Ok(books);
         }
-
-        [HttpGet ("Isbn")]
-        public async Task<Book> GetBook(string id)
-        {
-            using (var connection = new SqliteConnection(_connectionString))
-            {
-                await connection.OpenAsync();
-
-                var command = connection.CreateCommand();
-                command.CommandText = "SELECT Id, ISBN, Title, Author, Genre, Copies, AviCopies, PublishYear, AddDateTime, Images, RentCount FROM Books WHERE isbn = @id";
-                command.Parameters.AddWithValue("@id", id);
-
-                using (var reader = await command.ExecuteReaderAsync())
-                {
-                    if (await reader.ReadAsync())
-                    {
-                        return new Book
-                        {
-                            Id = reader.GetInt32(0),
-                            ISBN = reader.GetString(1),
-                            Title = reader.GetString(2),
-                            Author = reader.GetString(3),
-
-                            // Split the comma-separated Genre string into a List<string>
-                            Genre = reader.IsDBNull(4) ? new List<string>() : reader.GetString(4).Split(',').ToList(),
-
-                            Copies = reader.GetInt32(5),
-                            AviCopies = reader.GetInt32(6),
-                            PublishYear = reader.GetInt32(7),
-
-                            // Parse the AddDateTime from a string to DateTime
-                            AddDateTime = DateTime.Parse(reader.GetString(8)),
-
-                            // Split the comma-separated Images string into a List<string>
-                            Images = reader.IsDBNull(9) ? new List<string>() : reader.GetString(9).Split(',').ToList(),
-
-                            RentCount = reader.GetInt32(10)
-                        };
-                    }
-                }
-            }
-            return null;
-        }
-
-
-
-
-
-
-
-
-
-
     }
 }
-
