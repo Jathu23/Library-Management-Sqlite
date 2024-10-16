9ddff429a8005f939c96e9ba9f2b94886d5f80a4
Denojan A. <UT01397@unicomtic.com>
2024-10-16 11:53:16 +0530

    add lentrecode api on front

--- frontend/ScriptFiles/returnprocess.js
+++ frontend/ScriptFiles/returnprocess.js
@@ -1,3 +1,4 @@
+let rec_table = document.getElementById('return_rec_table');
 async function fetchreturnrecods() {
     try {
         let response = await fetch('https://localhost:7182/api/Return/GetAllRecods');
@@ -5,10 +6,43 @@ async function fetchreturnrecods() {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
         let data = await response.json();
-       console.log(data);
         return data;
     } catch (error) {
         console.error("An error occurred:", error.message);
     }
 }
-fetchreturnrecods();
+
+async function showReturnrecods() {``
+    try {
+        rec_table.innerHTML =`<tr>
+        <th>Return Id</th>
+        <th> User-NIC</th>
+        <th>Lent-Id</th>
+        <th>ISBN</th>
+        <th>LentCopies</th>
+        <th>ReturnCopies</th>
+        <th>ReturnDate</th>
+    </tr>`;
+
+let rec = await fetchreturnrecods();
+rec.forEach(r => {
+    rec_table.innerHTML += `
+    <tr>
+            <td>${r.id}</td>
+            <td>${r.usernic}</td>
+            <td>${r.lentId}</td>
+            <td>${r.isbn}</td>
+            <td>${r.lentcopies}</td>
+            <td>${r.returncopies}</td>
+            <td>${r.returndate}</td>
+        </tr>
+    `;
+});
+
+        
+    } catch (error) {
+        console.log(error,error.message);
+    }
+}
+
+showReturnrecods();
