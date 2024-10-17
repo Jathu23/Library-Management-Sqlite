5d6b0999fb6b8e06b504d915b70440c5fedede9c
Saruja T. <UT01372@unicomtic.com>
2024-10-17 13:55:18 +0530

    retun user detail get by api

--- frontend/ScriptFiles/returnprocess.js
+++ frontend/ScriptFiles/returnprocess.js
@@ -46,3 +46,105 @@ rec.forEach(r => {
 }
 
 showReturnrecods();
+
+
+
+document.getElementById('issue_userid').addEventListener('input', () => {
+    showUserDetails();
+})
+
+
+
+async function fetchSingleUser(NicNumber) {
+    try {
+        let url = 'https://localhost:7182/api/User/GetByUser?nic=' + encodeURIComponent(NicNumber);
+        let response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        let data = await response.json();
+        return data;
+
+    } catch (error) {
+        console.error("An error occurred:", error.message);
+    }
+}
+
+async function showUserDetails() {
+    try {
+        const NicNumber = document.getElementById('issue_userid').value;
+
+        if (NicNumber) {
+            const UserData = await fetchSingleUser(NicNumber);
+            console.log(UserData);
+            console.log(UserData.fullName);
+            console.log(UserData.phoneNumber);
+            console.log(UserData.joinDate);
+            console.log(UserData.email);
+            let displaydiv = document.getElementById('issue-user');
+
+            displaydiv.children[0].children[1].innerHTML = UserData.fullName
+            displaydiv.children[1].children[1].innerHTML = UserData.phoneNumber
+            displaydiv.children[2].children[1].innerHTML = UserData.email
+            displaydiv.children[3].children[1].innerHTML = UserData.joinDate
+            displaydiv.children[4].children[1].innerHTML = UserData.copies
+            displaydiv.children[5].children[1].innerHTML = `${futureDate}`
+        }
+
+    } catch (error) {
+        console.log("error in show user details");
+        console.log(error.message);
+    }
+
+}
+
+
+
+
+
+
+async function fetchSingleUserLend(NicNumber) {
+    try {
+        let url = 'https://localhost:7182/api/Lent/GetRecordsby_Nic?nic==' + encodeURIComponent(NicNumber);
+        let response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        let data = await response.json();
+        return data;
+
+    } catch (error) {
+        console.error("An error occurred:", error.message);
+    }
+}
+
+
+
+
+
+async function showLendRecordDetails() {
+    try {
+        const NicNumber = document.getElementById('issue_userid').value;
+
+        if (true) {
+            const endData = await fetchSingleUserLend(NicNumber) ;
+           
+
+            let displaydiv = document.getElementById('issue-info');
+
+            displaydiv.children[0].children[1].innerHTML = bookData.title
+            displaydiv.children[1].children[1].innerHTML = bookData.author
+            displaydiv.children[2].children[1].innerHTML = bookData.id
+            displaydiv.children[3].children[1].innerHTML = bookData.publishYear
+            displaydiv.children[4].children[1].innerHTML = bookData.copies
+            displaydiv.children[5].children[1].innerHTML = `${futureDate}`
+
+
+        }
+
+    } catch (error) {
+        console.log("error ");
+        console.log(error.message);
+    }
+
+}
