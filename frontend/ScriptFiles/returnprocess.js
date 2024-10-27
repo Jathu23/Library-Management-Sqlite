5a1f7b7652f0905ea8c0bef36adc5892d220e2dc
Jathushan <jathushanj2003@gmail.com>
2024-10-27 13:30:11 +0530

    work on recive book page

--- frontend/ScriptFiles/returnprocess.js
+++ frontend/ScriptFiles/returnprocess.js
@@ -12,9 +12,10 @@ async function fetchreturnrecods() {
         console.error("An error occurred:", error.message);
     }
 }
-async function showReturnrecods() {``
+async function showReturnrecods() {
+    ``
     try {
-        rec_table.innerHTML =`<tr>
+        rec_table.innerHTML = `<tr>
         <th>Return Id</th>
         <th> User-NIC</th>
         <th>Lent-Id</th>
@@ -24,9 +25,9 @@ async function showReturnrecods() {``
         <th>ReturnDate</th>
     </tr>`;
 
-let rec = await fetchreturnrecods();
-rec.forEach(r => {
-    rec_table.innerHTML += `
+        let rec = await fetchreturnrecods();
+        rec.forEach(r => {
+            rec_table.innerHTML += `
     <tr>
             <td>${r.id}</td>
             <td>${r.usernic}</td>
@@ -37,11 +38,11 @@ rec.forEach(r => {
             <td>${r.returndate}</td>
         </tr>
     `;
-});
+        });
 
-        
+
     } catch (error) {
-        console.log(error,error.message);
+        console.log(error, error.message);
     }
 }
 async function fetchSingleUserLend(NicNumber) {
@@ -93,40 +94,91 @@ async function showUserDetails() {
 
         if (NicNumber) {
             const UserData = await fetchSingleUser(NicNumber);
-            let displaydiv = document.getElementById('issue-user');
+            var displaydiv = document.getElementById('issue-user');
+            displaydiv.innerHTML = `  <div class="info">
+                        <h4>Full Name</h4>
+                        <p id="full-name">${UserData.fullName}</p>
+                    </div>
+                    <div class="info">
+                        <h4>Phone Number</h4>
+                        <p id="user-phone">${UserData.phoneNumber}</p>
+                    </div>
+                    <div class="info">
+                        <h4>Email</h4>
+                        <p id="user-email">${UserData.email}</p>
+                    </div>
+                    <div class="info">
+                        <h4>Join Date</h4>
+                        <p id="user-join-date">${UserData.joinDate}</p>
+                    </div>
+                    <div class="info">
+                        <h4>Pending Books</h4>
+                        <ul id="user_pending_books" style="margin-left: 20px;"></ul>
+                    </div>`;
 
-            displaydiv.children[0].children[1].innerHTML = UserData.fullName
-            displaydiv.children[1].children[1].innerHTML = UserData.phoneNumber
-            displaydiv.children[2].children[1].innerHTML = UserData.email
-            displaydiv.children[3].children[1].innerHTML = UserData.joinDate
-            displaydiv.children[4].children[1].innerHTML = pendingbooks
-        
+            var pending = document.getElementById('user_pending_books');
+            pendingbooks.forEach(book => {
+                pending.innerHTML += `<li>${book}</li>`;
+            });
+            if (pendingbooks.length == 0) {
+                pending.innerHTML += `No pending books`;
+            }
+
+
         }
 
     } catch (error) {
+        displaydiv.innerHTML = `<div class="info">
+                        <h4>Full Name</h4>
+                        <p id="full-name">--------</p>
+                    </div>
+                    <div class="info">
+                        <h4>Phone Number</h4>
+                        <p id="user-phone">--------</p>
+                    </div>
+                    <div class="info">
+                        <h4>Email</h4>
+                        <p id="user-email">-----------</p>
+                    </div>
+                    <div class="info">
+                        <h4>Join Date</h4>
+                        <p id="user-join-date">----------</p>
+                    </div>
+                    <div class="info">
+                        <h4>Pending Books</h4>
+                       <ul id="user_pending_books" style="margin-left: 20px;"></ul>
+                    </div>`;
         console.log("error in show user details");
         console.log(error.message);
     }
 
 }
 async function showLendRecordDetails(nic) {
-    try {
-      let infos = await getCustomInfo(nic)
-      custoninfodiv.innerHTML = ``;
-      infos.forEach(rec => {
-        custoninfodiv.innerHTML += ` <div>
-                        <p>${rec.lentId}</p>
-                        <p>${rec.bookTitle}</p>
-                        <p>${rec.copies}</p>
-                        <p>${rec.lentDate}</p>
-                        <p>${rec.status}</p>
-                        <button onclick="lodeinfo('${rec.lentId}')">select</button>
-                 </div>  </br>  `;
-   
-        
-      });
-     
+    console.log(nic);
 
+    if (!nic) {
+        custoninfodiv.innerHTML = `<h3>Enter user Id to lode data</h3> `;
+    }
+    try {
+        let infos = await getCustomInfo(nic);
+        console.log(infos);
+
+        if (infos.length == 0) {
+            custoninfodiv.innerHTML = `No data found`;
+        } else {
+            custoninfodiv.innerHTML = ``;
+            infos.forEach(rec => {
+                custoninfodiv.innerHTML += ` <div>
+                          <p>${rec.lentId}</p>
+                          <p>${rec.bookTitle}</p>
+                          <p>${rec.copies}</p>
+                          <p>${rec.lentDate}</p>
+                          <p>${rec.status}</p>
+                        <button onclick="lodeinfo('${rec.lentId}', '${rec.copies}','${nic}','${rec.bookTitle}')">select</button>
+
+                   </div>  </br>  `;
+            });
+        }
     } catch (error) {
         console.log("error ");
         console.log(error.message);
@@ -136,7 +188,7 @@ async function showLendRecordDetails(nic) {
 async function getCustomInfo(userId) {
     try {
         const url = `https://localhost:7182/api/Lent/customInfo?userid=${userId}`;
-        
+
         let response = await fetch(url);
 
         if (!response.ok) {
@@ -144,16 +196,40 @@ async function getCustomInfo(userId) {
         }
 
         let data = await response.json();
-        console.log("Custom Info:", data);
         return data;
 
     } catch (error) {
         console.error("Error fetching custom info:", error.message);
     }
 }
-function lodeinfo(lentid) {
-    alert(lentid);
+function lodeinfo(lentid, copies,userid,title) {
+    let count = document.getElementById('issue_bookcount');
+    let Rtitle = document.getElementById('issue_title');
+    count.value = copies;
+    Rtitle.value = title;
+
+    let receivebook = {
+        "id": setid(),
+        "usernic": Number(userid),
+        "lentId": Number(lentid),
+        "returncopies": Number( count.value)
+    }
+
+    let receiveBookButton = document.getElementById("recivebookbtn");
+
+    // Remove any existing click event listeners on the button
+    receiveBookButton.replaceWith(receiveBookButton.cloneNode(true));
+    receiveBookButton = document.getElementById("recivebookbtn");
+
+    // Add a new click event listener
+    receiveBookButton.addEventListener('click', () => {
+        console.log(receivebook);
+
+    });
 }
+function setid() {
+    return Number(Date.now().toString().substring(6));
+};
 
 
 
@@ -163,8 +239,10 @@ showReturnrecods();
 
 
 document.getElementById('issue_userid').addEventListener('input', () => {
-    showUserDetails();   
-    showLendRecordDetails(document.getElementById('issue_userid').value);
+    let id = document.getElementById('issue_userid').value;
+    showUserDetails();
+
+    showLendRecordDetails(id);
 });
 
 
